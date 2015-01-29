8e4d0ae43d6c211fa8b6aba389487a9bf61448ce
Omar Sandoval <osandov@osandov.com>
2015-01-28 17:04:45 -0800

    Sender that maybe works sometimes!
    Still have stutter issues, and need to rewrite stream handling w/o ring
    buffer.

--- fsk/sender.c
+++ fsk/sender.c
@@ -1,14 +1,28 @@
+#include <assert.h>
 #include <math.h>
 #include <portaudio.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "pa_ringbuffer.h"
+
+#define RING_BUFFER_SIZE (1 << 12)
 
 #define SAMPLE_RATE 44100
 #define FRAMES_PER_BUFFER paFramesPerBufferUnspecified
 
+#define BAUD 10
+#define ZERO_FREQ 1200.f
+#define ONE_FREQ 2200.f
+
 struct callback_data {
-	float phase;
+	PaUtilRingBuffer *ring_buffer;
+	int phase;
+	int frame;
+	int bit_index;
+	char byte;
+	bool bit;
 };
 
 static int send_callback(const void *input_buffer, void *output_buffer,
@@ -18,28 +32,66 @@ static int send_callback(const void *input_buffer, void *output_buffer,
 {
 	float *out = output_buffer;
 	struct callback_data *data = arg;
+	float frequency;
+	ring_buffer_size_t ret;
+	unsigned long i;
 
 	(void)input_buffer;
 	(void)time_info;
 	(void)status_flags;
 
-	for (unsigned long i = 0; i < frames_per_buffer; i++) {
-		*out++ = sinf(data->phase / 12);
-		*out++ = sinf(data->phase / 4);
+	for (i = 0; i < frames_per_buffer; i++) {
+		if (data->frame >= SAMPLE_RATE / BAUD) {
+			if (data->bit_index >= 8) {
+				ret = PaUtil_ReadRingBuffer(data->ring_buffer,
+							    &data->byte, 1);
+				if (ret == 0)
+					break;
+				data->bit_index = 0;
+			}
+			data->frame = 0;
+			data->bit = data->byte & (1 << data->bit_index);
+			data->bit_index++;
+		}
+		data->frame++;
+
+		frequency = data->bit ? ONE_FREQ : ZERO_FREQ;
+
+		out[i] = sinf(2 * M_PI * frequency * data->phase / SAMPLE_RATE);
 		data->phase++;
 	}
+
+	for (; i < frames_per_buffer; i++)
+		out[i] = 0.f;
 
 	return paContinue;
 }
 
 int main(void)
 {
+	PaUtilRingBuffer ring_buffer;
+	ring_buffer_size_t ring_ret;
+	void *ring_buffer_ptr;
 	PaStream *stream;
 	PaError err;
 	struct callback_data data = {
-		.phase = 0.f,
+		.ring_buffer = &ring_buffer,
+		.frame = SAMPLE_RATE / BAUD,
+		.bit_index = 8,
 	};
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t ret;
 	int status = EXIT_SUCCESS;
+
+	ring_buffer_ptr = malloc(RING_BUFFER_SIZE);
+	if (!ring_buffer_ptr) {
+		perror("malloc");
+		return EXIT_FAILURE;
+	}
+
+	PaUtil_InitializeRingBuffer(&ring_buffer, 1, RING_BUFFER_SIZE,
+				    ring_buffer_ptr);
 
 	err = Pa_Initialize();
 	if (err != paNoError) {
@@ -49,8 +101,7 @@ int main(void)
 		goto out;
 	}
 
-	/* Open an audio I/O stream. */
-	err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE,
+	err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, SAMPLE_RATE,
 				   FRAMES_PER_BUFFER, send_callback, &data);
 	if (err != paNoError) {
 		fprintf(stderr, "PortAudio: opening stream failed: %s\n",
@@ -67,7 +118,16 @@ int main(void)
 		goto close_stream;
 	}
 
-	Pa_Sleep(5000);
+	while ((ret = getline(&line, &n, stdin)) > 0) {
+		ring_ret = PaUtil_WriteRingBuffer(&ring_buffer, line,
+						  (ring_buffer_size_t)ret);
+		assert(ring_ret == (ring_buffer_size_t)ret);
+	}
+
+	if (ret == -1 && !feof(stdin)) {
+		perror("getline");
+		status = EXIT_FAILURE;
+	}
 
 	err = Pa_StopStream(stream);
 	if (err != paNoError) {
@@ -94,5 +154,7 @@ terminate:
 	}
 
 out:
+	free(line);
+	free(ring_buffer_ptr);
 	return status;
 }
