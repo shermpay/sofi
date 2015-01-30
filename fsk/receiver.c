7d327ab0f5d90cc8488cac5fe59903c32558df8d
Omar Sandoval <osandov@osandov.com>
2015-01-29 17:18:16 -0800

    Receiver cleanup

--- fsk/receiver.c
+++ fsk/receiver.c
@@ -12,49 +12,17 @@
 #define SAMPLE_RATE 44100
 #define FRAMES_PER_BUFFER paFramesPerBufferUnspecified
 
-#define SAMPLE_SILENCE 0.f
-
-#define BAUD 10
-#define ZERO_FREQ 1200.f
-#define ONE_FREQ 2200.f
-
-struct callback_data {
-	PaUtilRingBuffer *ring_buffer;
-	int phase;
-	int frame;
-	int bit_index;
-	char byte;
-	bool bit;
-};
-
 static int receive_callback(const void *input_buffer, void *output_buffer,
-			 unsigned long frames_per_buffer,
-			 const PaStreamCallbackTimeInfo *time_info,
-                            PaStreamCallbackFlags status_flags, void *arg)
+			    unsigned long frames_per_buffer,
+			    const PaStreamCallbackTimeInfo *time_info,
+			    PaStreamCallbackFlags status_flags, void *arg)
 {
-
-        struct callback_data *data = arg;
-
-	ring_buffer_size_t elements_writeable =
-		PaUtil_GetRingBufferWriteAvailable(data->ring_buffer);
-	ring_buffer_size_t elements_to_write =
-		fmin(elements_writeable, (ring_buffer_size_t)frames_per_buffer);
-	const float *in = (const float *) input_buffer;
-
 	(void)output_buffer;
 	(void)frames_per_buffer;
 	(void)time_info;
 	(void)status_flags;
 
-	unsigned long i;
-	if (in == NULL) {
-		float silence = SAMPLE_SILENCE;
-		for (i = 0; i < frames_per_buffer; i++) {
-			PaUtil_WriteRingBuffer(data->ring_buffer, &silence, 1);
-		}
-	} else {
-		PaUtil_WriteRingBuffer(data->ring_buffer, in, elements_to_write);
-	}
+	PaUtil_WriteRingBuffer(arg, input_buffer, frames_per_buffer);
 
         return paContinue;
 }
@@ -66,15 +34,8 @@ int main(void)
 	void *ring_buffer_ptr;
 	PaStream *stream;
 	PaError err;
-
-	struct callback_data data = {
-		.ring_buffer = &ring_buffer,
-		.frame = SAMPLE_RATE / BAUD,
-		.bit_index = 8,
-	};
-
 	int status = EXIT_SUCCESS;
-	
+
 	ring_buffer_ptr = malloc(RING_BUFFER_SIZE * sizeof(float));
 	if (!ring_buffer_ptr) {
 		perror("malloc");
@@ -93,7 +54,8 @@ int main(void)
 	}
 
 	err = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, SAMPLE_RATE,
-				   FRAMES_PER_BUFFER, receive_callback, &data);
+				   FRAMES_PER_BUFFER, receive_callback,
+				   &ring_buffer);
 	if (err != paNoError) {
 		fprintf(stderr, "PortAudio: opening stream failed: %s\n",
 			Pa_GetErrorText(err));
@@ -109,13 +71,12 @@ int main(void)
 		goto close_stream;
 	}
 
-	float x = 0.f;
 	while (true) {
-		ring_ret = PaUtil_ReadRingBuffer(data.ring_buffer, &x, 1);
-		if (ring_ret > 0) {
+		float x;
+
+		ring_ret = PaUtil_ReadRingBuffer(&ring_buffer, &x, 1);
+		if (ring_ret > 0)
 			printf("%f\n", x);
-		}
-		Pa_Sleep(1000);
 	}
 
 	err = Pa_StopStream(stream);
