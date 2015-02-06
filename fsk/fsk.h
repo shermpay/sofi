dbf7738ccb7a71d902aea60dbddbaa511d21af07
Omar Sandoval <osandov@osandov.com>
2015-02-05 17:00:42 -0800

    Add common code for message boundaries

--- fsk/fsk.h
+++ fsk/fsk.h
@@ -1,8 +1,17 @@
 #ifndef FSK_H
 #define FSK_H
 
+#include <inttypes.h>
+
 #define BAUD 50
 #define ZERO_FREQ 2200.f
 #define ONE_FREQ 1200.f
+
+#define INTERPACKET_GAP (2.f / BAUD)
+
+struct sofi_msg {
+	uint8_t len;
+	char payload[UINT8_MAX];
+};
 
 #endif /* FSK_H */
