c889981ec7745287998c7252dd74fdc89dac499f
Omar Sandoval <osandov@osandov.com>
2015-03-01 17:35:42 -0800

    Specify frequencies at runtime

--- sofinc.c
+++ sofinc.c
@@ -18,6 +18,8 @@
 
 #define M_PI 3.14159265359f
 
+static const char *progname = "sofinc";
+
 static int debug_mode;
 void debug_printf(int v, const char *format, ...)
 {
@@ -62,26 +64,31 @@ static inline float demod_window(void)
 
 /* Symbol definitions. */
 
-/* Size of a symbol in bits (must be 1, 2, 4, or 8). */
-#define SYMBOL_WIDTH 1
-#define NUM_SYMBOLS (1 << SYMBOL_WIDTH)
-#define SYMBOLS_PER_BYTE (CHAR_BIT / SYMBOL_WIDTH)
+/* Size of a symbol in bits. XXX: (must be 1, 2, 4, or 8). */
+static int symbol_width;
+
+static inline int num_symbols(void)
+{
+	return 1 << symbol_width;
+}
+
+static inline unsigned int symbols_per_byte(void)
+{
+	return CHAR_BIT / symbol_width;
+}
 
 /* Frequencies in Hz for each symbol value. */
-static const float symbol_freqs[NUM_SYMBOLS] = {
-	2200.f,		/* 0 */
-	1200.f,		/* 1 */
-};
+static float symbol_freqs[1 << 8];
 
 static inline unsigned int symbol_from_byte(unsigned char c, unsigned int i)
 {
-	unsigned int mask = ((1 << SYMBOL_WIDTH) - 1) << (SYMBOL_WIDTH * i);
-	return (c & mask) >> (SYMBOL_WIDTH * i);
+	unsigned int mask = ((1 << symbol_width) - 1) << (symbol_width * i);
+	return (c & mask) >> (symbol_width * i);
 }
 
 static inline unsigned char bits_from_symbol(unsigned int s, unsigned int i)
 {
-	return s << (SYMBOL_WIDTH * i);
+	return s << (symbol_width * i);
 }
 
 /* So-Fi state. */
@@ -107,7 +114,7 @@ enum receiver_state {
 };
 
 struct symbol_counts {
-	unsigned int symbols[NUM_SYMBOLS];
+	unsigned int symbols[1 << 8];
 	unsigned int silence;
 };
 
@@ -162,13 +169,13 @@ static int sofi_callback(const void *input_buffer, void *output_buffer,
 			data->sender.len = sizeof(data->sender.packet.len) + data->sender.packet.len;
 			data->sender.packet_index = 0;
 			data->sender.frame = SAMPLE_RATE / baud - 1;
-			data->sender.symbol_index = SYMBOLS_PER_BYTE - 1;
+			data->sender.symbol_index = symbols_per_byte() - 1;
 
 			data->sender.state = SEND_STATE_TRANSMITTING;
 			/* Fallthrough. */
 		case SEND_STATE_TRANSMITTING:
 			if (++data->sender.frame >= SAMPLE_RATE / baud) {
-				if (++data->sender.symbol_index >= SYMBOLS_PER_BYTE) {
+				if (++data->sender.symbol_index >= symbols_per_byte()) {
 					if (data->sender.packet_index >= data->sender.len) {
 						PaUtil_AdvanceRingBufferReadIndex(&data->sender.buffer,
 										  data->sender.packet.len);
@@ -239,7 +246,7 @@ static inline int strongest_symbol(const float *fs)
 	int max_symbol = -1;
 	float max_val = 0.f;
 
-	for (int i = 0; i < NUM_SYMBOLS; i++) {
+	for (int i = 0; i < num_symbols(); i++) {
 		/* XXX: need a real heuristic for silence. */
 		if (fs[i] > 100.f && fs[i] > max_val) {
 			max_val = fs[i];
@@ -260,7 +267,7 @@ static inline int calc_mostly(struct symbol_counts *counts)
 	int max = -1;
 	unsigned int max_count = counts->silence;
 
-	for (int i = 0; i < NUM_SYMBOLS; i++) {
+	for (int i = 0; i < num_symbols(); i++) {
 		if (counts->symbols[i] > max_count) {
 			max_count = counts->symbols[i];
 			max = i;
@@ -326,12 +333,12 @@ static void receiver_loop(PaUtilRingBuffer *buffer, float *window_buffer)
 	int n;
 	struct symbol_counts counts;
 	char byte = 0;
-	int symbol_index = 0;
+	unsigned int symbol_index = 0;
 	struct sofi_packet packet;
 	unsigned offset;
-	float sinfs[NUM_SYMBOLS];
-	float cosfs[NUM_SYMBOLS];
-	float fs[NUM_SYMBOLS];
+	float sinfs[1 << 8];
+	float cosfs[1 << 8];
+	float fs[1 << 8];
 	float time;
 	int symbol;
 
@@ -353,12 +360,12 @@ static void receiver_loop(PaUtilRingBuffer *buffer, float *window_buffer)
 		memset(sinfs, 0, sizeof(sinfs));
 		memset(cosfs, 0, sizeof(cosfs));
 		for (int i = 0; i < RECEIVER_WINDOW; i++) {
-			for (int j = 0; j < NUM_SYMBOLS; j++) {
+			for (int j = 0; j < num_symbols(); j++) {
 				sinfs[j] += sinf(2 * M_PI * symbol_freqs[j] * i / SAMPLE_RATE) * window_buffer[i];
 				cosfs[j] += cosf(2 * M_PI * symbol_freqs[j] * i / SAMPLE_RATE) * window_buffer[i];
 			}
 		}
-		for (int j = 0; j < NUM_SYMBOLS; j++)
+		for (int j = 0; j < num_symbols(); j++)
 			fs[j] = sinfs[j] * sinfs[j] + cosfs[j] * cosfs[j];
 		symbol = strongest_symbol(fs);
 
@@ -405,7 +412,7 @@ static void receiver_loop(PaUtilRingBuffer *buffer, float *window_buffer)
 				wait_until = t0 + (1.f / (2.f * baud)) + (n / baud) - (demod_window() / 2.f);
 
 				byte |= bits_from_symbol(mostly, symbol_index++);
-				if (symbol_index >= SYMBOLS_PER_BYTE) {
+				if (symbol_index >= symbols_per_byte()) {
 					if (state == RECV_STATE_LENGTH_GATHER) {
 						packet.len = (uint8_t)byte;
 					} else if (state == RECV_STATE_PAYLOAD_GATHER) {
@@ -440,8 +447,8 @@ static void receiver_loop(PaUtilRingBuffer *buffer, float *window_buffer)
 static void usage(bool error)
 {
 	fprintf(error ? stderr : stdout,
-		"Usage: sofinc [-d] [-w DEMOD_WINDOW] -b BAUD\n"
-		"       sofinc -h\n");
+		"Usage: %1$s [-d] [-f FREQ1,FREQ2,...] [-w DEMOD_WINDOW] -b BAUD\n"
+		"       %1$s -h\n", progname);
 	exit(error ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 
@@ -458,9 +465,13 @@ int main(int argc, char **argv)
 	int ret;
 	int opt;
 
-	while ((opt = getopt(argc, argv, "db:w:h")) != -1) {
+	if (argc > 0)
+		progname = argv[0];
+	while ((opt = getopt(argc, argv, "db:f:w:h")) != -1) {
 		char *end;
 		long temp;
+		float freq;
+		int i;
 
 		switch (opt) {
 		case 'b':
@@ -477,6 +488,33 @@ int main(int argc, char **argv)
 		case 'd':
 			debug_mode++;
 			break;
+		case 'f':
+			for (i = 0; i < 256; i++) {
+				if (*optarg == '\0')
+					usage(true);
+				freq = strtof(optarg, &end);
+				if (*end != '\0' && *end != ',')
+					usage(true);
+				symbol_freqs[i] = freq;
+				if (*end == '\0')
+					break;
+				else
+					optarg = end + 1;
+			}
+			if (i == 1) {
+				symbol_width = 1;
+			} else if (i == 3) {
+				symbol_width = 2;
+			} else if (i == 15) {
+				symbol_width = 4;
+			} else if (i == 255) {
+				symbol_width = 8;
+			} else {
+				fprintf(stderr, "%s: symbol width must be 1, 2, 4, or 8\n",
+					progname);
+				usage(true);
+			}
+			break;
 		case 'h':
 			usage(false);
 		default:
@@ -485,6 +523,12 @@ int main(int argc, char **argv)
 	}
 	if (baud < 1.f)
 		usage(true);
+
+	if (symbol_width == 0) {
+		symbol_width = 1;
+		symbol_freqs[0] = 2200.f;
+		symbol_freqs[1] = 1200.f;
+	}
 
 	/* Initialize callback data and receiver window buffer. */
 	sender_buffer_ptr = malloc(SENDER_BUFFER_SIZE);
